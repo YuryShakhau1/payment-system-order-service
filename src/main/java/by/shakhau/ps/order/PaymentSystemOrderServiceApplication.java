e7ef7466930374101b39b8a8f0a08c9898f500c7
Yury Shakhau <shakhauyury@gmail.com>
2026-08-05 15:40:48 +0300

    Created empty spring boot application

---
+++ src/main/java/by/shakhau/ps/order/PaymentSystemOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package by.shakhau.ps.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/shakhau/ps/order/PaymentSystemOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shakhau.ps.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
